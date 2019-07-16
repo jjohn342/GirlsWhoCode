081f16a3edd5b3e5df31ac760e20dbc9692b7efd
Jolina <jolinaemilyjohn@gmail.com>
2019-07-16 11:07:01 -0400

    book

---
+++ book.py
@@ -0,0 +1,8 @@
+favorite_books = ("little women", "the selection", "caraval")
+print (len(favorite_books))
+print (favorite_books[1])
+print ("caraval" in favorite_books)
+for item in favorite_books:
+    print (item)
+for num in range(len(favorite_books)):
+    print (favorite_books[num])
