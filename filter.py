1b0aade0e9f1dedcd3dd776a57e8d8bb178a0cb3
Jolina <jolinaemilyjohn@gmail.com>
2019-07-22 15:21:36 -0400

    h

---
+++ filter.py
@@ -0,0 +1,30 @@
+# Rename this file to be "filters.py"
+# Add commands to import modules here.
+from PIL import Image
+
+# Define your load_img() function here.
+#       Parameters: The name of the file to be opened (string)
+#       Returns: The image object with the opened file.
+def load_img(pic):
+    im = Image.open(pic)
+    return im
+
+# Define your show_img() function here.
+#       Parameters: The image object to display.
+#       Returns: nothing.
+def show_img(pic):
+    pic.show()
+
+# Define your save_img() function here.
+#      2 Parameters: The image object to save, the name to save the file as (string)
+#       Returns: nothing.
+def save_img(pic, summer):
+    pic.save(summer)
+
+# Define your obamicon() function here.
+#       Parameters: The image object to apply the filter to.
+#       Returns: A New Image object with the filter applied.
+# def obamicon(pic):
+im = load_img("download.jfif")
+show_img(im)
+save_img(im, "HappySummer.jfif")
