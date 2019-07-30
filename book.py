1c3c63e91d9a1df31725bed6eab276ded103066d
Jolina <jolinaemilyjohn@gmail.com>
2019-07-30 15:30:04 -0400

    n

--- book.py
+++ book.py
@@ -6,3 +6,8 @@ for item in favorite_books:
     print (item)
 for num in range(len(favorite_books)):
     print (favorite_books[num])
+another_list = ("6", "2", "6")
+another_list = ("purple", "red", "orange")
+for item in another_list:
+    print (item)
+print (another_list)
